7a16a8aa3158c1084e812e8d5430b3bc5efac652
Nozimjon Nabiev <nozimjonnabiev7@gmail.com>
2023-11-09 23:05:12 +0500

    Initial commit

---
+++ api/src/main/java/com.nozimjon.api/App.java
@@ -0,0 +1,9 @@
+package com.nozimjon.api;
+
+import com.nozimjon.core.Utils;
+
+public class App {
+    public static void main(String[] args) {
+        Utils.isAllPositiveNumbers("12", "79");
+    }
+}
